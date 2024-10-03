48057e58529e511fd601083e08d755dcf1af48ef
AbdelrahmanMohammed002 <114600073+AbdelrahmanMohammed002@users.noreply.github.com>
2024-10-03 20:04:21 +0300

    number guissing game

---
+++ Number Guissing Game.py
@@ -0,0 +1,36 @@
+import random
+
+# Get range limit from user input
+stop_range = input("Enter range limit: ")
+
+# Validate input
+if not stop_range.isdigit() or int(stop_range) <= 0:
+    print("Please enter a number greater than 0.")
+    quit()
+
+stop_range = int(stop_range)
+
+# Generate a random number within the given range
+random_number = random.randint(0, stop_range)
+guess_score = 0
+
+# Start the guessing game loop
+while True:
+    guess_score += 1
+    user_guess = input("Make a guess: ")
+
+    if not user_guess.isdigit() or int(user_guess) <= 0:
+        print("Please re-enter a number greater than 0.")
+        continue
+
+    user_guess = int(user_guess)
+
+    if user_guess == random_number:
+        print("You got it!")
+        break
+    elif user_guess > random_number:
+        print("You are above the number!")
+    else:
+        print("You are below the number!")
+
+print(f"You got it in {guess_score} attempts.")
