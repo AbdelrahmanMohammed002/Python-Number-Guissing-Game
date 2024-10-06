47786fd9d74a1e328956db56b1ef18e4a0476058
AbdelrahmanMohammed002 <114600073+AbdelrahmanMohammed002@users.noreply.github.com>
2024-10-06 08:58:47 +0300

    number guess game

--- Number Guissing Game.py
+++ Number Guissing Game.py
@@ -1,36 +1,45 @@
 import random
 
-# Get range limit from user input
-stop_range = input("Enter range limit: ")
+Playing = True
 
-# Validate input
-if not stop_range.isdigit() or int(stop_range) <= 0:
-    print("Please enter a number greater than 0.")
-    quit()
+while Playing:
 
-stop_range = int(stop_range)
+    # Get range limit from user input
+    stop_range = input("Enter range limit: ")
 
-# Generate a random number within the given range
-random_number = random.randint(0, stop_range)
-guess_score = 0
+    # Validate input
+    if not stop_range.isdigit() or int(stop_range) <= 0:
+        print("Please enter a number greater than 0.")
+        quit()
 
-# Start the guessing game loop
-while True:
-    guess_score += 1
-    user_guess = input("Make a guess: ")
+    stop_range = int(stop_range)
 
-    if not user_guess.isdigit() or int(user_guess) <= 0:
-        print("Please re-enter a number greater than 0.")
-        continue
+    # Generate a random number within the given range
+    random_number = random.randint(0, stop_range)
+    guess_score = 0
 
-    user_guess = int(user_guess)
+    # Start the guessing game loop
+    while True:
+        guess_score += 1
+        user_guess = input("Make a guess: ")
 
-    if user_guess == random_number:
-        print("You got it!")
-        break
-    elif user_guess > random_number:
-        print("You are above the number!")
-    else:
-        print("You are below the number!")
+        if not user_guess.isdigit() or int(user_guess) <= 0:
+            print("Please re-enter a number greater than 0.")
+            continue
 
-print(f"You got it in {guess_score} attempts.")
+        user_guess = int(user_guess)
+
+        if user_guess == random_number:
+            print("You got it!")
+            break
+        elif user_guess > random_number:
+            print("You are above the number!")
+        else:
+            print("You are below the number!")
+
+    print(f"You got it in {guess_score} attempts.")
+
+    choice = input("DO you want to play again (yes/no): ").lower()
+
+    if choice != "yes":
+        Playing = False
